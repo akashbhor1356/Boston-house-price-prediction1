82e02a92035f8e1df2cacdba9bf969edbd57b8a3
sushant8525 <105856571+sushant8525@users.noreply.github.com>
2022-10-14 18:49:55 +0530

    gfgf

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import pickle
-from tkinter import Scale
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
-from numpy.core.multiarray import array
 import pandas as pd
  
 app=Flask(__name__)
@@ -12,12 +10,13 @@ regmodel=pickle.load(open('regmodel.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/predict_api',methods=['POST'])
 def pridict_api():
     data= request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalr.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data= np._ScalarType.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
