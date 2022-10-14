74f82a73808a5fd4d5f7e8359015d25cf818afa9
sushant8525 <105856571+sushant8525@users.noreply.github.com>
2022-10-14 20:23:45 +0530

    final

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import numpy as np
 import pandas as pd
  
 app=Flask(__name__)
+
 regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
 
 
 @app.route('/')
@@ -16,10 +18,22 @@ def pridict_api():
     data= request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data= np._ScalarType.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data= scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="the house price prediction is {}".format(output))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
 
 if __name__=='__main__':
     app.run(debug=True)
