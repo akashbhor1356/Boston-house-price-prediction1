cf6c537ebce07984739120835aa774b0409328fb
sushant8525 <105856571+sushant8525@users.noreply.github.com>
2022-10-16 08:51:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import json
 import pickle
-
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
@@ -36,5 +35,3 @@ def predict():
 
 if __name__=="__main__":
     app.run(debug=True)
-
-
